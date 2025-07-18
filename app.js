6d591c08e29f7e154f238416b2c32d1f6bd83b86
amitjipancholi <amitjipancholi@gmail.com>
2025-07-19 01:23:04 +0530

    base testing done ,we will start working on project

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB connected"))
+}).then(() => {
+  console.log("✅ MongoDB connected");
+  app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`+' '+`http://localhost:${PORT}`));
+})
   .catch(err => console.error("❌ MongoDB connection failed:", err));
 
 // Example route
 app.get('/', (req, res) => res.send("API running"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server started on port ${PORT}`));
+
+
